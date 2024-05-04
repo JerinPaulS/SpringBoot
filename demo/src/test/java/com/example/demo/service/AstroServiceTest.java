58ae977006f624333d789510df906fbc26e5d9fb
JerinPaulS <jerinsprograms@gmail.com>
2024-05-04 22:21:56 +0530

    RestTemplate

---
+++ demo/src/test/java/com/example/demo/service/AstroServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.services.AstroService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class AstroServiceTest {
+
+    @Autowired
+    private AstroService service;
+
+    @Test
+    void getPeopleInSpaceTest() {
+        String people = service.getPeopleInSpace();
+        assertTrue(people.contains("people"));
+    }
+
+    @Test
+    void getAstroResponse() {
+        var response = service.getAstroResponse();
+        assertAll(
+                () -> assertEquals("success", response.message()),
+                () ->assertTrue(response.number() >= 0),
+                () -> assertEquals(response.people().size(), response.number())
+        );
+    }
+}
